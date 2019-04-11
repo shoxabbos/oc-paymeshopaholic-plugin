cdad66d275b0be322d526d46494e586cca71a0f9
Shoxabbos <19941001@inbox.ru>
2019-04-11 10:58:32 +0500

    Update routes.php

--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 use Illuminate\Http\Request;
 use Shohabbos\Paymeshopaholic\Classes\PaymeHandler;
 
-Route::any('/payme-webhook', function (Request $request) {
+Route::any('/shohabbos/paymeshopaholic/webhook', function (Request $request) {
     $handler = new PaymeHandler();
     return $handler->listen($request);
 });
